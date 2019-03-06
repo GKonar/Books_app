86556e30b98c1728269f2da6619c003f550d3e99
Grzegorz Konarski <grzegorz.konarski86@gmail.com>
2019-03-06 18:44:03 +0100

    Add basic features to the app: showAlert, clearFields, deleteBook, addBookToList and connect it to the UI

--- app.js
+++ app.js
@@ -16,11 +16,51 @@ UI.prototype.addBookToList = function (book) {
     const row = document.createElement('tr');
 
     // Insert cols 
-    row.innerHTML = `<td>${book.title}</td>`;
-    
+    row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href="#" class="delete">X</a></td>
+    `;
+
+    list.appendChild(row);
 }
 
-// Event Listeners
+// Show Alert
+UI.prototype.showAlert = function (message, className) {
+    // Create div
+    const div = document.createElement('div');
+    // Add classes 
+    div.className = `alert ${className}`;
+    // Add text
+    div.appendChild(document.createTextNode(message));
+    // Get parent
+    const container = document.querySelector('.container');
+    const form = document.querySelector('#book-form');
+    // Insert Alert
+    container.insertBefore(div, form); // what we wanna insert and second argument what we want to insert before
+
+    // Timeout after 3 sec
+    setTimeout(function(){
+        document.querySelector('.alert'). remove();
+    }, 3000)
+}
+
+// Delete Book
+UI.prototype.deleteBook = function(target) {
+    if(target.className === 'delete') {
+        target.parentElement.parentElement.remove(); // event delegation
+    }
+}
+
+UI.prototype.clearFields = function() {
+    // Clearing fields
+    document.getElementById('title').value = '',
+    document.getElementById('author').value = '',
+    document.getElementById('isbn').value = '';
+}
+
+// Event Listeners for add book
 document.getElementById('book-form').addEventListener('submit', function(e) {
     // Get form values from the UI
     const title = document.getElementById('title').value,
@@ -33,8 +73,35 @@ document.getElementById('book-form').addEventListener('submit', function(e) {
     // Instantiate UI
     const ui = new UI();
 
-    // Add book to list
-    ui.addBookToList(book);
+    // Validate
+    if(title === '' || author === '' || isbn === '' ) {
+        // Error alert 
+        ui.showAlert('Please fill in all fields' , 'error')
+    } else {
+        // Add book to list
+        ui.addBookToList(book);
+
+        // Show success 
+        ui.showAlert('Book Added!' , 'success ');
+
+        // Clear Fields
+        ui.clearFields();
+    }
 
     e.preventDefault();
 })
+
+// Event Listener for delete book
+document.getElementById('book-list').addEventListener('click', function(e) {
+    // Instantiate UI
+    const ui = new UI();
+
+    // Delete Book
+    ui.deleteBook(e.target);
+
+    // Show message 
+    ui.showAlert('Book Removed!',  'success');
+
+    e.preventDefault();
+});
+
